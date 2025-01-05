26e833bb22120c63233a3c5b73d4e3089dbd4c71
zoesacks <zoesacks@gmail.com>
2025-01-05 11:20:46 -0300

    modificacion de las rutas

--- conf/gunicorn_config.py
+++ conf/gunicorn_config.py
@@ -1,5 +1,5 @@
-command = '/home/ubuntu/aplicaciones/venv/bin/gunicorn'
-pythonpath = '/home/ubuntu/aplicaciones/gestor_pedidos'
+command = '/root/aplicaciones/venv/bin/gunicorn'
+pythonpath = '/root/aplicaciones/gestor_pedidos'
 
 bind = '0.0.0.0:8000'
 workers = 3
