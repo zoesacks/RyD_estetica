17f64fd793eb7bbbfa86305efcbeabfcaee56b1e
zoesacks <zoesacks@gmail.com>
2025-05-13 10:51:47 -0300

    ajustes estaticos

--- gestor_pedidos/settings.py
+++ gestor_pedidos/settings.py
@@ -119,12 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
 STATIC_URL = '/static/'
+
 if DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")  # <- carpeta para archivos recolectados
 else:
     STATIC_ROOT = '/root/aplicaciones/gestor_pedidos/static/'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
